3e603b4f1c0f1da3ab618f0a244c505d468e79b4
nicolasportela <2103@holbertonschool.com>
2020-10-26 20:30:06 +0000

    fix

--- _printf.c
+++ _printf.c
@@ -9,40 +9,46 @@
  */
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0, fi, speci;
-spec_t spec[] = {
-{"c", print_char}, {"s", print_str},
-{"%", print_per}, {"d", print_int},
-{"i", print_int}, {"b", print_bin},
-{"o", print_oct}, {"X", print_Xhexa},
-{"u", print_u}, {NULL, NULL}
-};
+	va_list args;
+	int count = 0, fi, speci;
+	spec_t spec[] = {
+		{"c", print_char}, {"s", print_str},
+		{"%", print_per}, {"d", print_int},
+		{"i", print_int}, {"b", print_bin},
+		{"o", print_oct}, {"X", print_Xhexa},
+		{"u", print_u}, {NULL, NULL}
+	};
 
-va_start(args, format);
-if (format == NULL)
-return (-1);
-for (fi = 0; format[fi] != '\0'; fi++)
-{
-while (format[fi] != '%' && format[fi] != '\0')
-{
-_putchar(format[fi]);
-count++;
-fi++;
-}
-if (format[fi] != '\0')
-fi++;
-else
-break;
-for (speci = 0; speci < 9; speci++)
-{
-if (format[fi] == *(spec[speci].spec))
-{
-count += spec[speci].f(args);
-break;
-}
-}
-}
-va_end(args);
-return (count);
+	va_start(args, format);
+       	if (format[0] == '%' || format[1] == '\0')
+	{
+		return (-1);
+	}
+	for (fi = 0; format[fi] != '\0'; fi++)
+	{
+		while (format[fi] != '%' && format[fi] != '\0')
+		{
+			_putchar(format[fi]);
+			count++;
+			fi++;
+		}
+		if (format[fi] != '\0')
+		{
+			fi++;
+		}
+		else
+		{
+			break;
+		}
+		for (speci = 0; speci < 9; speci++)
+		{
+			if (format[fi] == *(spec[speci].spec))
+			{
+				count += spec[speci].f(args);
+				break;
+			}
+		}
+	}
+	va_end(args);
+	return (count);
 }
