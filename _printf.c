3921b3ab7427e54b2c166480e78c7d7d20c2a3fb
alexoreiro <alexis.oreiro@icloud.com>
2020-10-27 11:43:25 +0000

    printf

--- _printf.c
+++ _printf.c
@@ -9,40 +9,50 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0, fi, speci;
-	spec_t spec[] = {
-		{"c", print_char}, {"s", print_str},
-		{"%", print_per}, {"d", print_int},
-		{"i", print_int}, {"b", print_bin},
-		{"o", print_oct}, {"X", print_Xhexa},
-		{"u", print_u}, {NULL, NULL}
-	};
+va_list args;
+int count = 0, fi, speci;
+spec_t spec[] = {
+{"c", print_char}, {"s", print_str},
+{"%", print_per}, {"d", print_int},
+{"i", print_int}, {"b", print_bin},
+{"o", print_oct}, {"X", print_Xhexa},
+{"u", print_u}, {NULL, NULL}
+};
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-	va_start(args, format);
-	for (fi = 0; format[fi] != '\0' && format; fi++)
-	{
-		while (format[fi] != '%' && format[fi] != '\0')
-		{
-			_putchar(format[fi]);
-			count++;
-			fi++;
-		}
-		if (format[fi] != '\0')
-			fi++;
-		else
-			break;
-		for (speci = 0; speci < 9; speci++)
-		{
-			if (format[fi] == *(spec[speci].spec))
-			{
-				count += spec[speci].f(args);
-				break;
-			}
-		}
-	}
-	va_end(args);
-	return (count);
+if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+return (-1);
+va_start(args, format);
+for (fi = 0; format[fi] != '\0' && format; fi++)
+{
+while (format[fi] != '%' && format[fi] != '\0')
+{
+_putchar(format[fi]);
+count++;
+fi++;
+}
+if (format[fi] != '\0')
+fi++;
+else
+break;
+for (speci = 0; speci < 9; speci++)
+{
+if (format[fi] == *(spec[speci].spec))
+{
+count += spec[speci].f(args);
+break;
+}
+}
+if (format[i+1] == ' ' || format[i+1] == '\0')
+{
+return (-1);
+}
+else
+{
+_putchar('%');
+_putchar(i+1);
+count == count + 2;
+}
+}
+va_end(args);
+return (count);
 }
